3b44a2094e6622323293b63d9ff5a051b72d67c6
Ciaran48 <ciaran2170@gmail.com>
2022-01-14 22:43:21 +0000

    Updated read me file Completion of the first programme of the course

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+    public static void main(String [] args){
+        System.out.println("Hello, Ciaran");
+
+        int num1 = (5+10) + (2*10);
+        int num2 = 6;
+        int num3 = num1*2;
+        int total = num1 + num2 + num3;
+        int newTotal = total - 1000;
+
+        System.out.println(total);
+        System.out.println(newTotal);
+
+
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Java-Udemy-Course
 Udemy course progress to help me upskill my java programming.
+
+Course can be found at https://github.com/Ciaran48/Java-Udemy-Course.git
+
+IDE IntelliJ
+JDK Amazon Corretto Version 11.0.13
