abde95840d8aee380032d4d6f2335bde6a6faf84
Ciaran48 <ciaran2170@gmail.com>
2022-01-19 17:05:34 +0000

    Completed the Strings Section

--- Strings/src/accademy/learnprogramming/Main.java
+++ Strings/src/accademy/learnprogramming/Main.java
@@ -11,5 +11,29 @@ public class Main {
         //double
         //char
         //boolean
+
+        //Strings,
+        //immutable - can't change after created, new string is created by java
+        //Code is inefficient using String as the same variable as seen below
+        //basic
+        String myString = "This is a String";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2022";
+        System.out.println("myString is equal to " + myString);
+
+        //numeric
+        String numberString = "250";
+        numberString = numberString + "250";
+        System.out.println(numberString);
+
+        //Addition
+        //doesn't work same with other numeric data types
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString is equal to " + lastString);
+
     }
 }
