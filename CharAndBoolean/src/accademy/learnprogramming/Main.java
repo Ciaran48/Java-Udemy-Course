f96f860f64fe8b3e9052054ef17eba61d73b75a0
Ciaran48 <ciaran2170@gmail.com>
2022-01-19 16:45:24 +0000

    Complete Char and Boolean Section

---
+++ CharAndBoolean/src/accademy/learnprogramming/Main.java
@@ -0,0 +1,24 @@
+package accademy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //char,
+        //2 bytes or 16 bits
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myUnicodeChar2 = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myUnicodeChar2);
+
+        //boolean
+        //true of false, yes or no, 1 or 0
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverEighteen = true;
+
+    }
+}
