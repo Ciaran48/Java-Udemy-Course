b27216a8221c7a6783aff268d70a2548bc5abb0d
Ciaran48 <ciaran2170@gmail.com>
2022-01-19 23:01:28 +0000

    Completed Sections
    and or operators,  assignment vs equal operators and if then

--- Operators/src/accademy/learnprogramming/Main.java
+++ Operators/src/accademy/learnprogramming/Main.java
@@ -52,10 +52,34 @@ public class Main {
         //if then statement, execute code if
         //conditional statement
         boolean isAlien = true;
-        if (isAlien == false) {
+        if (isAlien == false)
             System.out.println("It is not an alien!");
             System.out.println("I am afraid of aliens");//prints without a code block
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
         }
-        
+
+        //Logical and OR operators
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        //assignment vs equal to operators
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
     }
 }
