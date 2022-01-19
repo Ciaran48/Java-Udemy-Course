31f094d658bccb97b9be398b7963f132dfa32186
Ciaran48 <ciaran2170@gmail.com>
2022-01-19 16:45:44 +0000

    Beginning Strings Section

---
+++ Strings/src/accademy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+package accademy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+    }
+}
