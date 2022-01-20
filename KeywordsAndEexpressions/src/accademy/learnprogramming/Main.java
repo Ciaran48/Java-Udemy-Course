1c44706b2ae72498eacc7b5fb5725842236e7e2c
Ciaran48 <ciaran2170@gmail.com>
2022-01-20 14:20:18 +0000

    Expression Complete

---
+++ KeywordsAndEexpressions/src/accademy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package accademy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Expressions
+        //Write down the expressions of the code below
+        int score = 100;
+        if (score > 99){
+            System.out.println("You got the high score!");
+            score=0;
+        }
+
+        //Answers
+        //"score = 100"
+        //"score > 99"
+        //"you got the high score"
+        //"score = 0"
+    }
+}
