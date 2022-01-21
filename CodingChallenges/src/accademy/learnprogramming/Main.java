03310aff22e37c5dbcb2d4f5b88c4a70d10cc60c
ciaran adams <ciaran2170@gmail.com>
2022-01-21 11:22:59 +0000

    updated main for sum checker

--- CodingChallenges/src/accademy/learnprogramming/Main.java
+++ CodingChallenges/src/accademy/learnprogramming/Main.java
@@ -2,6 +2,7 @@ package accademy.learnprogramming;
 
 import static accademy.learnprogramming.BarkingDog.shouldWakeUp;
 import static accademy.learnprogramming.DecimalComparator.areEqualByThreeDecimalPlaces;
+import static accademy.learnprogramming.EqualSumChecker.hasEqualSum;
 import static accademy.learnprogramming.LeapYearCalculator.isLeapYear;
 import static accademy.learnprogramming.MegaBytesConverter.printMegaBytesAndKiloBytes;
 import static accademy.learnprogramming.SpeedConverter.printConversion;
@@ -35,6 +36,9 @@ public class Main {
 
         //DecimalComparator
         //System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+
+        //EqualSumChecker
+        //System.out.println(hasEqualSum(1, -1, 0));
 
     }
 
