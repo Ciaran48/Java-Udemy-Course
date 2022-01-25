68218a1affd443923fb07032d1675cf025236c6f
ciaran adams <ciaran2170@gmail.com>
2022-01-25 22:21:29 +0000

    LastDigitChecker Challenge Complete

--- CodingChallenges2/src/accademy/learnprogramming/LastDigitChecker.java
+++ CodingChallenges2/src/accademy/learnprogramming/LastDigitChecker.java
@@ -1,4 +1,18 @@
 package accademy.learnprogramming;
 
+/*Write a method named hasSameLastDigit with three parameters of type int.
+
+        Each number should be within the range of 10 (inclusive) - 1000 (inclusive). If one of the numbers is not within the range, the method should return false.
+
+        The method should return true if at least two of the numbers share the same rightmost digit; otherwise, it should return false.*/
 public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+        if(isValid(num1) && isValid(num2) && isValid(num3)){
+            return (num1%10==num2%10 || num1%10==num3%10 || num2%10==num3%10);
+        }
+        return false;
+    }
+    public static boolean isValid(int num){
+        return (num>=10 && num<=1000);
+    }
 }
