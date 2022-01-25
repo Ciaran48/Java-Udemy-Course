16883711f491e5db06220ae808c3ba34dc413287
ciaran adams <ciaran2170@gmail.com>
2022-01-25 13:46:11 +0000

    Completed the DoAndWgileStatements Section

---
+++ DoAndWhileStatements/src/accademy/learnprogramming/Main.java
@@ -0,0 +1,18 @@
+package accademy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        while(count !=5){
+            System.out.println("Count value is " + count);
+            count ++;
+        }
+
+        do {
+            System.out.println("Count value was " + count);
+            count++;
+        } while (count !=6);
+    }
+}
