aff9a5806265d20d0a57efab29a9e40003d26de5
ciaran adams <ciaran2170@gmail.com>
2022-01-25 12:40:21 +0000

    Completed the sum3And5Challenge

---
+++ CodingChallenges2/src/accademy/learnprogramming/Sum3And5Challenge.java
@@ -0,0 +1,28 @@
+package accademy.learnprogramming;
+/*create a for statement using a range of numbers from 1-1000
+sum all of the numbers that can be divided with both 3 and also with 5
+For those numbers that met the above conditions, print out the number
+break out of the loop once you find 5 numbers that met the above conditions
+After breaking out of the loop print the sum of the numbers that me the conditions*/
+
+public class Sum3And5Challenge {
+
+    //all code must be in main method in this case
+    /*int spotted = 0;
+    int total = 0;
+        for (int i = 1; i > 0 || i < 1000; i++) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            System.out.println(i);
+            spotted += 1;
+            total += i;
+            if (spotted == 5) {
+                break;
+            }
+        }
+
+    }
+        System.out.println("The total is " + total);*/
+
+
+}
