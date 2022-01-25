8a409ad3a56571696c82196a12d000e593915f30
ciaran adams <ciaran2170@gmail.com>
2022-01-25 21:21:22 +0000

    EvenDigitSum Challenge Complete

--- CodingChallenges2/src/accademy/learnprogramming/EvenDigitSum.java
+++ CodingChallenges2/src/accademy/learnprogramming/EvenDigitSum.java
@@ -1,4 +1,26 @@
 package accademy.learnprogramming;
 
+/*Write a method named getEvenDigitSum with one parameter of type int called number.
+
+        The method should return the sum of the even digits within the number.
+
+        If the number is negative, the method should return -1 to indicate an invalid value.*/
 public class EvenDigitSum {
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int digit = 0;
+        int sum = 0;
+        while (number > 0) {
+            digit = number % 10;
+            number = number / 10;
+            if (digit % 2 == 0) {
+                sum += digit;
+            }
+        }
+        return sum;
+    }
+
 }
