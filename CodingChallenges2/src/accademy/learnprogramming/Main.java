8a2272dd8d6627f927b2258ebac91fd3d63d12d9
ciaran adams <ciaran2170@gmail.com>
2022-01-26 16:22:08 +0000

    Challenges Tests Updated

--- CodingChallenges2/src/accademy/learnprogramming/Main.java
+++ CodingChallenges2/src/accademy/learnprogramming/Main.java
@@ -1,12 +1,20 @@
 package accademy.learnprogramming;
 
 import static accademy.NumberInWord.printNumberInWord;
+import static accademy.learnprogramming.AllFactors.printFactors;
 import static accademy.learnprogramming.DayOfTheWeekChallenge.printDayOfTheWeek;
 import static accademy.learnprogramming.DayOfTheWeekChallenge.printDayOfTheWeekBonus;
 import static accademy.learnprogramming.DigitSumChallenge.sumDigits;
+import static accademy.learnprogramming.EvenDigitSum.getEvenDigitSum;
+import static accademy.learnprogramming.FirstAndLastDigitSum.sumFirstAndLastDigit;
+import static accademy.learnprogramming.GreatestCommonDivisor.getGreatestCommonDivisor;
 import static accademy.learnprogramming.IsEvenNumber.isEvenNumber;
+import static accademy.learnprogramming.LastDigitChecker.hasSameLastDigit;
+import static accademy.learnprogramming.LastDigitChecker.isValid;
 import static accademy.learnprogramming.NumberOfDaysInAMonth.getDaysInMonth;
 import static accademy.learnprogramming.NumberOfDaysInAMonth.isLeapYear;
+import static accademy.learnprogramming.NumberPalindrome.isPalindrome;
+import static accademy.learnprogramming.SharedDigit.hasSharedDigit;
 import static accademy.learnprogramming.SumOdd.sumOdd;
 
 public class Main {
@@ -55,8 +63,48 @@ public class Main {
         System.out.println(sumDigits(-101));
         System.out.println(sumDigits(100800700));*/
 
+        /*NumberPalindromeChallenge
+        System.out.println(isPalindrome(-101));
+        System.out.println(isPalindrome(101));
+        System.out.println(isPalindrome(-123));
+        System.out.println(isPalindrome(123));*/
 
+        /*FirstAndLastDigitSumChallenge
+        System.out.println(sumFirstAndLastDigit(1001));
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(-10));*/
 
+        /*EvenDigitSumChallenge
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));*/
 
+        /*SharedDigitChallenge
+        System.out.println(hasSharedDigit(10,11));
+        System.out.println(hasSharedDigit(10,10));
+        System.out.println(hasSharedDigit(02,21));
+        System.out.println(hasSharedDigit(-11,11));*/
+
+        /*LastDigitCheckerChallenge
+        System.out.println(hasSameLastDigit (9, 99, 999));
+        System.out.println(hasSameLastDigit (23, 32, 42));
+        System.out.println(hasSameLastDigit (41, 22, 71));
+
+        System.out.println(isValid(10));
+        System.out.println(isValid(468));
+        System.out.println(isValid(1051));*/
+
+        /*GreatestCommonDivisorChallenge
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));*/
+
+        /*AllFactorsChallenge
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);*/
     }
 }
